9ad384ad0aae8ddd789cb5179ab4a6ee8999f526
prony <a.pronin@a-r-s.dev>
2024-10-11 16:06:00 +0000

    Add sample of server

--- src/teleop.py
+++ src/teleop.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
 import rospy
+import random
 from geometry_msgs.msg import Twist
 from getkey import getkey
 from std_srvs.srv import Empty
+from turtlesim.srv import Spawn
+from mgtu_anm24.srv import Go, GoResponse
 
 target = [0, 0]
 
 class Teleop:
+    tc = 0
+
     def __init__(self) -> None:
         rospy.loginfo("Hi!")
         rospy.Timer(rospy.Duration(0.1),self._t)
         self.pub = rospy.Publisher("/turtle1/cmd_vel", Twist)
+        self.go_serv = rospy.Service('go',Go, self.cb_serv_go)
 
     def _t(self, event):
         global target
@@ -29,6 +35,24 @@ class Teleop:
         except Exception as e:
             rospy.logwarn("Service call error: '%s'", e)
 
+    def new(self):
+        try:
+            req = rospy.ServiceProxy('spawn', Spawn)
+            self.tc+=1
+            res = req(
+                random.randrange(0,11)+random.randrange(0,100)/100,
+                random.randrange(0,11)+random.randrange(0,100)/100,
+                random.randrange(0,314)/100, 
+                f"turtle_{self.tc}"
+            )
+        except Exception as e:
+            rospy.logwarn("Service call error: '%s'", e)
+
+    def cb_serv_go(self, value):
+        resp = GoResponse()
+        resp.length = 1
+        resp.time = 2
+        return resp
 
 if __name__ == '__main__':
     rospy.init_node('mgtu_teleop')
@@ -44,5 +68,8 @@ if __name__ == '__main__':
             target[0] = -1
         if (key=='w') or (key=='8'):
             target[0] = 1
-        if (key=='r'):
+        if (key=='c'):
             teleop.clear()
+        if (key=='n'):
+            teleop.new()
+        
